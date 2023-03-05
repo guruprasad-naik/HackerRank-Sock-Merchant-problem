6295192651aa476417ead8fa50b1615d1816fc4e
Guruprasad Naik <56538327+guruprasad-naik@users.noreply.github.com>
2023-03-05 19:13:35 +0530

    Update and rename c code to HackerRank-Sock-Merchant-problem.c

--- HackerRank-Sock-Merchant-problem.c
+++ HackerRank-Sock-Merchant-problem.c
@@ -1,3 +1,4 @@
+/*
 # HackerRank-Sock-Merchant-problem
 John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
@@ -30,7 +31,7 @@ Sample Input
 Sample Output
 
 3
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
 #include<stdio.h>
 int main()
 {
